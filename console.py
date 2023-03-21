d088ae0f16471edd1f540b33bd2bffbd73a59abb
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-03-21 18:01:55 -0300

    console with modifications in do_create method

--- console.py
+++ console.py
@@ -137,7 +137,7 @@ class HBNBCommand(cmd.Cmd):
                 #param[1] = re.sub('[]')
             new_instance.__setattr__(param[0], param[1])
             print(new_instance.param[0])
-        
+    
 
     def help_create(self):
         """ Help information for the create method """
