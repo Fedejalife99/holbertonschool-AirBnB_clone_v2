658141ef2fbc3304c77e019e9c2655999dbdeb21
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-03-22 23:59:39 -0300

    task 3 and 4 fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,4 @@
 -- script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db'
-CREATE USER 'hbnb_test'@'localhost' IDENTITY BY 'hbnb_dev_pwd'
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev_db'@'localhost'
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev_db'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
