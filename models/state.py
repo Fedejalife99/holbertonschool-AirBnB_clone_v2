290a67e7d263e2ec031fb6c8930b972da98c65ec
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-03-25 11:21:33 -0300

    state.py changes in realtioship

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ class State(BaseModel, Base):
     
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+        cities = relationship("City", back_ref="state", cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
