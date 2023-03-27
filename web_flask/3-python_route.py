9dda0bb9d7be3dba4b18709d654153ab2fe6de3f
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-03-27 14:38:41 -0300

    task 3 second python decorator add

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c_var(text):
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text):
     return "python {}".format(text.replace("_", " "))
 
